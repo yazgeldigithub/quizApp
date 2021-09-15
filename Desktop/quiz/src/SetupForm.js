547372a9a9679ecac48fad137446cc33f45221d0
yazgeldigithub <devster2016@gmail.com>
2021-09-15 14:01:24 -0500

    final

--- Desktop/quiz/src/SetupForm.js
+++ Desktop/quiz/src/SetupForm.js
@@ -2,7 +2,86 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const SetupForm = () => {
-  return <h2>setup form</h2>
+  const { quiz, handleChange, handleSubmit, error } = useGlobalContext()
+  return (
+    <main>
+      <section className='quiz quiz-small'>
+        <form className='setup-form'>
+          <h2>setup quiz</h2>
+          {/* amount */}
+          <div className='form-control'>
+            <label htmlFor='amount'>number of questions</label>
+            <input
+              type='number'
+              name='amount'
+              id='amount'
+              value={quiz.amount}
+              onChange={handleChange}
+              className='form-input'
+              min={1}
+              max={50}
+            />
+          </div>
+          {/* category */}
+
+          <div className='form-control'>
+            <label htmlFor='category'>category</label>
+            <select
+              name='category'
+              id='category'
+              className='form-input'
+              value={quiz.category}
+              onChange={handleChange}
+            >
+              <option value='any'>any</option>
+              <option value='sports'>sports</option>
+              <option value='history'>history</option>
+              <option value='politics'>politics</option>
+              <option value='general_knowledge'>general knowledge</option>
+              <option value='books'>books</option>
+              <option value='film'>film</option>
+              <option value='music'>music</option>
+              <option value='musical_theatres'>musical theatres</option>
+              <option value='television'>television</option>
+              <option value='video_games'>video games</option>
+              <option value='board_games'>board games</option>
+              <option value='science_nature'>science nature</option>
+              <option value='computers'>computers</option>
+              <option value='math'>math</option>
+              <option value='geography'>geography</option>
+              <option value='art'>art</option>
+              <option value='celebrities'>celebrities</option>
+              <option value='animals'>animals</option>
+            </select>
+          </div>
+          {/* difficulty */}
+
+          <div className='form-control'>
+            <label htmlFor='difficulty'>select difficulty</label>
+            <select
+              name='difficulty'
+              id='difficulty'
+              className='form-input'
+              value={quiz.difficulty}
+              onChange={handleChange}
+            >
+              <option value='easy'>easy</option>
+              <option value='medium'>medium</option>
+              <option value='hard'>hard</option>
+            </select>
+          </div>
+          {error && (
+            <p className='error'>
+              can't generate questions, please try different options
+            </p>
+          )}
+          <button type='submit' onClick={handleSubmit} className='submit-btn'>
+            start
+          </button>
+        </form>
+      </section>
+    </main>
+  )
 }
 
 export default SetupForm
--- Desktop/quiz/src/context.js
+++ Desktop/quiz/src/context.js
@@ -5,16 +5,126 @@ const table = {
   sports: 21,
   history: 23,
   politics: 24,
+  general_knowledge: 9,
+  books: 10,
+  film: 11,
+  music: 12,
+  musical_theatres: 13,
+  television: 14,
+  video_games: 15,
+  board_games: 16,
+  science_nature: 17,
+  computers: 18,
+  math: 19,
+  mythology:20,
+  geography: 22,
+  art: 25,
+  celebrities: 26,
+  animals: 27,
+  any: ""
 }
 
 const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
 const url = ''
-
+const tempUrl =
+  'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [waiting, setWaiting] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [index, setIndex] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const [error, setError] = useState(false)
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    category: 'sports',
+    difficulty: 'easy',
+  })
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const fetchQuestions = async (url) => {
+    setLoading(true)
+    setWaiting(false)
+    const response = await axios(url).catch((err) => console.log(err))
+    if (response) {
+      const data = response.data.results
+      if (data.length > 0) {
+        setQuestions(data)
+        setLoading(false)
+        setWaiting(false)
+        setError(false)
+      } else {
+        setWaiting(true)
+        setError(true)
+      }
+    } else {
+      setWaiting(true)
+    }
+  }
+
+  const nextQuestion = () => {
+    setIndex((oldIndex) => {
+      const index = oldIndex + 1
+      if (index > questions.length - 1) {
+        openModal()
+        return 0
+      } else {
+        return index
+      }
+    })
+  }
+  const checkAnswer = (value) => {
+    if (value) {
+      setCorrect((oldState) => oldState + 1)
+    }
+    nextQuestion()
+  }
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+  const closeModal = () => {
+    setWaiting(true)
+    setCorrect(0)
+    setIsModalOpen(false)
+  }
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setQuiz({ ...quiz, [name]: value })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const { amount, category, difficulty } = quiz
+
+    const url = `${API_ENDPOINT}amount=${amount}&difficulty=${difficulty}&category=${table[category]}&type=multiple`
+    fetchQuestions(url)
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        error,
+        isModalOpen,
+        nextQuestion,
+        checkAnswer,
+        closeModal,
+        quiz,
+        handleChange,
+        handleSubmit,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
 }
 // make sure use
 export const useGlobalContext = () => {
